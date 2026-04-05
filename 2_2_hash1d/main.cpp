57b8ba28ba6caab613f1daa27f45c3d5363d3379
mtakagi <mtakagi127@live.jp>
2026-04-05 20:38:51 +0900

    add 2_2_hash1d

---
+++ 2_2_hash1d/main.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <vector>
+#include <bit>
+#include <cstdint>
+#include "color.h"
+#include "vec2.h"
+#include "vec3.h"
+
+constexpr uint k = 0x456789abu;
+
+uint uhash11(uint n) {
+    n ^= (n << 1);
+    n ^= (n >> 1);
+    n *= k;
+    n ^= (n << 1);
+
+    return n * k;
+}
+
+float hash11(float p) {
+    auto n = std::bit_cast<uint>(p);
+
+    return static_cast<float>(uhash11(n)) / std::numeric_limits<unsigned int>::max();
+}
+
+int main() {
+    const auto aspect_ratio = 16.0 / 9.0;
+    const int width = 384;
+    const int height = static_cast<int>(width / aspect_ratio);
+    auto time = 0.0f;
+
+    std::cout << "P3\n" << width << ' ' << height << "\n255\n";
+
+    for (int j = height - 1; j >= 0; --j) {
+        std::cerr << "\rScan lines remaining: " << j << ' ' << std::flush;
+        for (int i = 0; i < width; ++i) {
+            auto u = (double)i / (width - 1);
+            auto v = (double)j / (height - 1);
+            auto pos = vec2(u, v) + time;
+            auto hash = hash11(pos.x());
+            auto color = vec3(hash, hash, hash);          
+
+            write_color(std::cout, color);
+        }
+    }
+    std::cerr << "\nDone.\n";
+    
+    return 0;
+}
